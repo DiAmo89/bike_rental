15586e697fa7d5fead63ec434b7f3dc146a23fc4
Dmitrii Evdokimov <evdokimovbest@gmail.com>
2026-03-26 11:22:44 +0100

    added domain res.cloudinary.com to next.config

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "pro-bike.ru", pathname: "/**" },
-     
+
       { protocol: "https", hostname: "media.cdn.kaufland.de", pathname: "/**" },
-     
+
       { protocol: "https", hostname: "bikesonline.com", pathname: "/**" },
-      { protocol: "https", hostname: "www.buybestgear.com", pathname: "/**" }, 
+      { protocol: "https", hostname: "www.buybestgear.com", pathname: "/**" },
+      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
     ],
   },
 };
